d803bca1af61bc7ba0ee2a7f9fd2e5cd1fcf2dd5
kelley_h <hujian@hisens.cc>
2019-04-30 10:08:48 +0800

    test

--- rpc/client/main_micro_client.go
+++ rpc/client/main_micro_client.go
@@ -7,7 +7,7 @@ import (
 	"github.com/micro/go-micro"
 	pb "win-his/api"
 )
-
+// 这两个main内部实现是一样的
 func main() {
 	// create a new service
 	service := grpc.NewService()
@@ -25,8 +25,27 @@ func main() {
 		return
 	}
 	fmt.Println(workrsp.Msg)
-	msgresp, err := cl.PushMsg(ctx, &pb.PushMsgReq{
+	msgresp, err := cl.PushMsg(context.TODO(), &pb.PushMsgReq{
 		Username: "John",
 	})
 	fmt.Println(msgresp.Msg)
+}
+func main1() {
+
+	server := grpc.NewService()
+	server.Init()
+	client := pb.NewAdminService("Admin", server.Client())
+
+	wresp, err := client.TakeWorking(context.TODO(), &pb.Request{Username: "John"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(wresp)
+	mresp, err := client.PushMsg(context.TODO(), &pb.PushMsgReq{Username: "John"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(mresp)
 }
